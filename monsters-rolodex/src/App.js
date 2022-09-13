4002d2b4cb010d55efd61a0928acc8de24491f13
Diego Neves <pdcassiano@gmail.com>
2022-09-13 13:25:23 -0300

    Entendendo estados e suas mudanças

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,22 +4,29 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'Yihua',
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>Olá, {this.state.name}</p>
+          <button
+            onClick={() => {
+              if (this.state.name === 'Yihua') {
+                this.setState({ name: 'Andrei' })
+              } else {
+                this.setState({ name: 'Yihua' })
+              }
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     )
