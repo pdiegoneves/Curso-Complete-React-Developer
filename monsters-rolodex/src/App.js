7a9d3bb66db1ca5f3c7456fa6939b7e6eb0ff4ed
Diego Neves <pdcassiano@gmail.com>
2022-09-26 22:40:07 -0300

    buscando dados da api

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,43 +1,42 @@
-import { Component } from 'react'
+import { Component } from "react";
 
-import logo from './logo.svg'
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '9281iub191'
-        },
-        {
-          name: 'Frank',
-          id: '90189812hjbh1b'
-        },
-        {
-          name: 'Jacky',
-          id: '91812jb12ih12'
-        },
-        {
-          name: 'Andrei',
-          id: '1u7b88ahbhu1'
-        }
-      ]
-    }
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}><h1>{monster.name}</h1></div>
-          })
-        }
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
