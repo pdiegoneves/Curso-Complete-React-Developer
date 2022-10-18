fd289eb060e2733ab31ad67e237f9ba516b14d4c
Diego Neves <pdcassiano@gmail.com>
2022-10-18 06:09:41 -0300

    chore: refactory - extraction card component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox 
           className='monsters-search-box'
           placeholder='search monsters'
