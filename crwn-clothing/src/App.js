a115fba0b21636bb14313f78365093178fb9aa34
Diego Neves <pdcassiano@gmail.com>
2022-10-22 06:35:56 -0300

    chore: extracted component CategoryItem of app.js

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,7 @@
+import CategoryItem from './components/category-item/category-item.component'
+
 import './categories.styles.scss'
+ 
 
 const App = () => {
   const categories = [
@@ -31,16 +34,8 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map(( category ) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   )
