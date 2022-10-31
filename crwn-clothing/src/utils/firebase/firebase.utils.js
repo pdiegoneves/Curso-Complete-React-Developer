364f884206b0a682567adc3d3f5f13dee58bae67
Diego Neves <pdcassiano@gmail.com>
2022-10-31 05:52:06 -0300

    chore: iniciado config do firebase

---
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+    authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+    projectId: 'crwn-clothing-db-98d4d',
+    storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+    messagingSenderId: '626766232035',
+    appId: '1:626766232035:web:506621582dab103a4d08d6',
+}
+  
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = GoogleAuthProvider()
+provider.setCustomParameters({
+    prompt: "select_account",
+})
+
+export const auth = getAuth()
