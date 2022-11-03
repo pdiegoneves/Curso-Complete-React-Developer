909207c4858e58f76f7cbb34eba6bf0e2f9f2af5
Diego Neves <pdcassiano@gmail.com>
2022-11-03 06:23:54 -0300

    chore: ajust auth

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,19 @@
-import { signInWithGooglePopup, creatUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import {
+  signInWithGooglePopup,
+  creatUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+
   const logGoggleUser = async () => {
     const { user } = await signInWithGooglePopup()
-    creatUserDocumentFromAuth(user)
+    const userDocRef = await creatUserDocumentFromAuth(user)
   }
 
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoggleUser}>
-        Sign in with Google Popup
-      </button>
+      <button onClick={logGoggleUser}>Sign in with Google Popup</button>
     </div>
   )
 }
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -13,13 +13,14 @@ const firebaseConfig = {
   
 const firebaseApp = initializeApp(firebaseConfig)
 
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
     prompt: "select_account",
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+// export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 const db = getFirestore()
 
