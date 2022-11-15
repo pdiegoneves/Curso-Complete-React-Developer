3dd9094414ec0b738dac33ea19cf3a622efd56f4
Diego Neves <pdcassiano@gmail.com>
2022-11-15 08:11:02 -0300

    chore: add style

--- crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import { createAuthUserWithEmailAndPassword , creatUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
 
+import './sign-up.styles.scss'
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -49,8 +51,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input
