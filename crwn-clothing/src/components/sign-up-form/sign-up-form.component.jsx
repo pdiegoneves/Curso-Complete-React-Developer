ac8b1aa7cbc517a40d37b00e85673607ca0fe89b
Diego Neves <pdcassiano@gmail.com>
2022-11-15 08:39:49 -0300

    feat: created button component

--- crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,7 @@ import {
   creatUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
 
 import './sign-up.styles.scss'
 
@@ -90,7 +91,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   )
